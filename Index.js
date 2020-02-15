969b523ec2275df8d0141d17d75dcdd681a2380b
mfs9408 <59794609+mfs9408@users.noreply.github.com>
2020-02-15 23:19:11 +0300

    del

--- Index.js
+++
@@ -1,9 +0,0 @@
-var slides = document.querySelectorAll('#slider .slider');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 5000);
- 
-function nextSlide() {
-    slides[currentSlide].className = 'slider';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slider showing';
-}
